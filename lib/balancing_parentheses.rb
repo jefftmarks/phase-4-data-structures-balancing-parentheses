2aa5255b600e1a5148f65a6ce15b5d8ee1fbe837
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-21 13:07:24 -0400

    commit tree 29e7ba6e1743609bf6c3148fdde13ab9cc3e3b80

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,48 @@
 require_relative './stack'
 
-# your code here
+require 'pry'
+
+def balancing_parentheses(str)
+	stack = Stack.new
+
+	# str.chars.each do |value|
+	# 	if stack.size == 0 || stack.peek == ")"
+	# 		stack.push(value)
+	# 	elsif value == ")"
+	# 		stack.pop
+	# 	else
+	# 		stack.push(value)
+	# 	end
+	# end
+
+	str.chars.each do |value|
+		if stack.peek == "(" && value == ")"
+			stack.pop
+		else
+			stack.push(value)
+		end
+	end
+
+	stack.size
+
+end
+
+def balancing_parentheses_2(str)
+	unclosed = 0
+	unopened = 0
+
+	str.chars.each do |value|
+		if value == "("
+			unclosed += 1
+		elsif unclosed > 0
+			unclosed -= 1
+		else
+			unopened += 1
+		end
+	end
+
+	unclosed + unopened
+
+end
+
+# binding.pry
